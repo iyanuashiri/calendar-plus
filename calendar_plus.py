bc2b4697e5f444d29840a852ee604752309770cc
Ashiri <ajaoiyanu@gmail.com>
2019-03-21 13:27:40 +0100

    Override formatday function

---
+++ calendar_plus.py
@@ -0,0 +1,20 @@
+from calendar import HTMLCalendar
+
+
+class ModelCalendar(HTMLCalendar):
+
+    def __init__(self, year=None, month=None):
+        self.year = year
+        self.month = month
+        super(ModelCalendar, self).__init__()
+
+    def formatday(self, day, query):
+        objects_per_day = query
+        d = ''
+
+        for object_ in objects_per_day:
+            d += '<li>{0}</li>'.format(object_.get_html_url)
+
+        if day != 0:
+            return "<td><span>{0}</span><ul> {1} </ul></td>".format(day, d)
+        return '<td></td>'
